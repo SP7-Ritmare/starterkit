c101f93ddc8c54107cfd0f2bb3ee95f8b0d46be4
menegon <ste.menegon@gmail.com>
2013-12-04 11:59:11 +0100

    add getdata url

--- geosk/urls.py
+++ geosk/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 
     # Static pages
     url(r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
+    url(r'^mdtools/api/getdata$', 'geosk.mdtools.views.get_data_api', name='mdtools_getdata'),
  ) + urlpatterns
