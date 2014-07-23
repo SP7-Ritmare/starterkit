8f1df6756601648ded4a979543b0ecbcb729c804
menegon <ste.menegon@gmail.com>
2014-07-24 00:03:12 +0200

    add InsertObservation POST

--- geosk/osk/api.py
+++ geosk/osk/api.py
@@ -3,18 +3,24 @@ import os
 import json
 import requests
 from lxml import etree
+from urlparse import urlsplit
+
+from owslib.namespaces import Namespaces
+from owslib.util import nspath_eval
+
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.safestring import mark_safe
-from urlparse import urlsplit
+
 from geosk.skregistration.views import get_key
 from geonode.utils import http_client, _get_basic_auth_info, json_response
-from django.core.urlresolvers import reverse
-
 from geosk.osk.models import Sensor
+
+namespaces = Namespaces().get_namespaces('ows110')
 
 def lastediml(request):
     s = Sensor.objects.order_by('-id').all()[0]
@@ -74,13 +80,35 @@ def sensormlproxy(request):
         ediml = etree.fromstring(request.raw_post_data)
         fileid = ediml.find('fileId').text
 
-        # save sensorml & edi xml
-        sensor = Sensor(fileid=fileid)
+        headers = {
+            'Accept': 'application/xml',
+            'Content-Type': 'application/xml',
+            'Authorization': "%s" % settings.SOS_SERVER['default']['TRANSACTIONAL_AUTHORIZATION_TOKEN']
+            }
+
+        settings.SOS_SERVER['default']['LOCATION']
+        sos_response = requests.post(
+            settings.SOS_SERVER['default']['LOCATION'] + '/pox', 
+            data=sensorml,  headers=headers, 
+            verify=False
+            )
+
+        
+        if sos_response.status_code == 200:
+            tr = etree.fromstring(sos_response.content)
+            if tr.tag == nspath_eval("ows110:ExceptionReport", namespaces):
+                return json_response(exception=sos_response.text.encode('utf8'), status=500)
+
+            # save sensorml & edi xml
+            sensor = Sensor(fileid=fileid)
     
-        sensor.sensorml = sensorml
-        sensor.ediml = request.raw_post_data
-        sensor.save()
-        return json_response(body={'success':True,'redirect': reverse('osk_browse')})
+            sensor.sensorml = sensorml
+            sensor.ediml = request.raw_post_data
+            sensor.save()
+            return json_response(body={'success':True,'redirect': reverse('osk_browse')})
+        
+        else:
+            return json_response(exception=sos_response.text.encode('utf8'), status=500)
 
     return json_response(errors='Cannot create SensorML', 
                          status=500)
--- geosk/osk/proxy.py
+++ geosk/osk/proxy.py
@@ -22,13 +22,13 @@ class ObservationsProxy(HttpProxy):
             return HttpResponseForbidden('Forbidden')
         # force authorization header to allow "Transactional authorization token"
         if request.user.has_perm('osk.admin_sos'):
-            request.META['HTTP_AUTHORIZATION'] = "%s" % settings.SOS_TRANSACTIONAL_AUTHORIZATION_TOKEN
+            request.META['HTTP_AUTHORIZATION'] = "%s" % settings.SOS_SERVER['default']['TRANSACTIONAL_AUTHORIZATION_TOKEN']
         else:
             request.META.pop('HTTP_AUTHORIZATION', None)
-
         return super(ObservationsProxy, self).dispatch(request, *args, **kwargs)
 
     def check_perms(self, request):
         if settings.SOS_PUBLIC_ACCESS or request.user.has_perm('osk.read_sos') or request.user.has_perm('osk.admin_sos'):
             return True
         return False
+
