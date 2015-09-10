546ef74a8a67d5f7c77bfab176fa90ed436906f3
menegon <ste.menegon@gmail.com>
2015-09-10 11:14:00 +0200

    new version

--- geosk/__init__.py
+++ geosk/__init__.py
@@ -19,7 +19,7 @@
 
 import os
 # __version__ = (1, 2, 0, 'alpha', 6)
-__version__ = (1, 2, 13, 'final', 0)
+__version__ = (1, 2, 14, 'final', 0)
 
 class UnregisteredSKException(Exception):
     pass
