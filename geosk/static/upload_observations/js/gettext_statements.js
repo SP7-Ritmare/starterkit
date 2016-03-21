03082a645f550ef5f18ac570ad3ef9443a0d4592
ptagliolato <ptagliolato@users.noreply.github.com>
2016-03-21 11:03:34 +0100

    gettext statements file to feed i18n mechanism for preparing .po files

---
+++ geosk/static/upload_observations/js/gettext_statements.js
@@ -0,0 +1,53 @@
+/**
+ *  gettext statements file for get-it starterkit upload_observations
+ *  Intended application: feed i18n mechanism for preparing .po files
+ *  author: Paolo Tagliolato - CNR IREA in Milano - www.irea.cnr.it
+ *  created on on 21/03/16.
+ *
+ */
+
+gettext("Select Procedure");// "Seleziona sensore";
+gettext("Select Location");// "Seleziona posizione";
+gettext("Insert observations");// "Inserisci osservazioni";
+gettext("Select a Procedure/Sensor");// "Seleziona procedura/sensore";
+gettext("A sensor, system, method, algorithm or instrument which may be used in making an Observation (which may be described using SensorML)");// "Un sensore, sistema, metodo, algoritmo o strumento utilizzabile per effettuare osservazioni (descrvibile eventualmente usando SensorML)";
+gettext("Select one of the available \"Features of Interest\" or create a new one");// "Seleziona uno degli Oggetti di Interesse disponibili o creane uno nuovo";
+gettext("In the first instance the Feature Of Interest (and the Sampling Feature) describe the geographical location of the target of the Observation, but more generally a Feature Of Interest is an abstraction of real world phenomena (thing, species, person, event, geographic place, etc.).");// "L'Oggetto di Interesse (Feature of Interest), insieme all'Oggetto Campione (Sampling Feature), descrive qui la posizione geografica dell'oggetto dell'osservazione, ma più in generale un Oggetto di Interesse è un'astrazione di un fenomeno del mondo reale (cosa, specie, persona, evento, luogo geografico)";
+gettext("Name");// "Nome";
+gettext("A descriptive name for the Feature Of Interest");// "Un nome descrittivo per l'Oggetto di Interesse";
+gettext("Sampling point");// "Punto di campionamento";
+gettext("Sampling Point");// "Punto di campionamento";
+gettext("The sampling position");// "Posizione puntuale del campionamento";
+gettext("Sampled Feature (URI)");// "URI dell'oggetto geografico campionato (sampled feature)";
+gettext("Is the feature of the Sampling Feature, providing the ultimate context for the Observation. A Sampling Feature provides the direct context for the specific Observation (spatial, specimen etc.).");// "E' l'Oggetto (geografico) Campionato (sampled feature). L'osservazione è invece effettuata su un secondo oggetto detto Oggetto Campione (sampling feature).";
+gettext("Action");// "Azione";
+gettext("Lat.");// "Lat.";
+gettext("Lon.");// "Lon.";
+gettext("SRS");// "SRS";
+gettext("You can use the map to choose (by left click) a Sampled Feature from the local WFS (activate the WFS layers with the layers control in the map)");// "Puoi usare la mappa per scegliere (tasto sinistro) l'Oggetto Campionato dal WFS locale (per attivare i layer WFS usa il controllo in alto a sinistra sulla mappa)";
+gettext("Use");// "Usa";
+gettext("Tips");// "Suggerimenti";
+gettext("You can input the coordinates for a new feature right clicking on the map");// "Puoi inserire le coordinate per un nuovo punto di campionamento facendo click con il tasto destro sulla mappa";
+gettext("You can use the map to choose (by left click) a Sampled Feature from the local WFS");// "Puoi usare la mappa per scegliere (tasto sinistro) l'Oggetto Campionato da uno dei layer del WFS locale";
+gettext("Features of Interest already inserted for the chosen Sensor/Procedure are displayed on the map. Click their marker to obtain more info");// "I punti di campionamento relativi agli Oggetti di Interesse precedentemente inseriti per la procedura/sensore selezionata sono mostrati sulla mappa. Per ottenerne informazioni fare click sul loro segnaposto";
+gettext("Use the control layer on the map to show/hide layers");// "Usa lo strumento di controllo presente sulla mappa per nascondere o mostrare i layer";
+gettext("Insert data");// "Inserisci i dati";
+gettext("Fill in the data manually or paste them from a spreadsheet, without headings.\n\t\t      \t\t\t\t\tPlease check the order of columns here proposed.\n\t\t      \t\t\t\t\t");// "Inserisci i dati manualmente o fai copia-incolla da un foglio di calcolo  senza copiare la riga di intestazione (per i copia-incolla verifica prima la corrispondenza con l'ordine delle colonne proposte in questa pagina!)";
+gettext("Show accepted date-time formats.");// "Mostra i formati di data e ora accettati";
+gettext("REMARK - accepted date-time formats.");// "NOTA BENE: formati di data e ora accettati";
+gettext("Timestamp fields (date and time) like \"phenomenon time\" or \"result time\" require values expressed in ISO 8601 format. The table in this page admits also date and time time values in one of the following formats");// "Campi di data e ora come \"istante del fenomeno\" o \"istante del risultato\" richiedono valori espressi in formato ISO 8601. La tabella in questa pagina ammette anche i formati di data e ora seguenti";
+gettext(" that will be automatically converted in the standard format. The use of different timestamps formats could lead to unexpected errors. Please check the conversion before submitting your data.");// "che verranno automaticamente convertiti nel formato standard. L'uso di formati diversi da quelli indicati potrebbe condurre ad errori inattesi. Si prega di verificare la conversione automatica prima salvare i dati.";
+gettext("save data");// "Salva i dati";
+gettext("ResultTime");// "Istante del Risultato (Result Time)";
+gettext("Please check the result time (i.e. the time when the result became available). Change it if different from the last phenomenonTime (default).");// "Si prega di verificare l'istante del risultato (ossia: l'istante in cui il risultato è diventato disponibile): dopo l'inserimento delle osservazioni esso viene automaticamente compilato copiando l'ultimo istante del fenomeno. Se le osservazioni inserite necessitano di distinguere il momento del fenomeno dal momento in cui i valori delle osservazioni si sono resi disponibili (per esempio in seguito a un'analisi di laboratorio) è possibile cambiare tale valore.";
+gettext("reset table");// "Cancella la tabella";
+gettext("Procedure/Sensor");// "Procedura/Sensore";
+gettext("Feature of Interest");// "Oggetto di Interesse";
+gettext("Sampled Feature");// "Oggetto Campionato";
+gettext("Available Features of Interest<br/>for the current procedure");// "Oggetti di Interesse precedentemente inseriti per la procedura selezionata";
+gettext("Set this as the sampling position<br/> for the new feature");// "Imposta questa come posizione di campionamento per un nuovo Oggetto di Interesse";
+gettext("Use this as the sampled feature");// "Usa questo come Oggetto Campionato";
+gettext("Legend - Fields definition");// "Legenda - definizione dei campi";
+gettext("Quantity");// "Quantità";
+gettext("Date and Time");// "Data e Ora";
+gettext("Unit of measure");// "Unità di Misura";
