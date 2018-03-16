33e3116503f26706f2c875a3bd9a6aca4959c57c
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-03-16 21:36:23 +0100

    Add scripts for test dumping and loading data

---
+++ scripts/docker/misc/test_docker_loaddata_settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+/usr/local/bin/invoke update >> /usr/src/app/invoke.log
+
+source $HOME/.override_env
+
+echo DATABASE_URL=$DATABASE_URL
+echo GEODATABASE_URL=$GEODATABASE_URL
+echo SITEURL=$SITEURL
+echo ALLOWED_HOSTS=$ALLOWED_HOSTS
+echo GEOSERVER_PUBLIC_LOCATION=$GEOSERVER_PUBLIC_LOCATION
+
+python manage.py loaddata /tmp/mdtools.json --settings=geosk.settings
+
