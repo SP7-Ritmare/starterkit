04c692fbb78c5ef4ff1ec765f6f06e48a6765f0a
afabiani <alessio.fabiani@gmail.com>
2021-11-05 11:12:57 +0000

    Fix docker build

--- src/setup.py
+++ src/setup.py
@@ -63,7 +63,7 @@ class PostInstallCommand(install):
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, '../README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
