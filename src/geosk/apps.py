494aec51cf3c6b095acadc9e26a34b7e741495a1
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2022-05-12 10:37:03 +0200

    Revert "Sos integration"

--- src/geosk/apps.py
+++ src/geosk/apps.py
@@ -5,33 +5,12 @@ from django.apps import AppConfig as BaseAppConfig
 def run_setup_hooks(*args, **kwargs):
     from django.conf import settings
     from .celeryapp import app as celeryapp
-    from django.conf.urls import include, url
-    from geonode.urls import urlpatterns
 
     LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
     settings.TEMPLATES[0]["DIRS"].insert(0, os.path.join(LOCAL_ROOT, "templates"))
 
     if celeryapp not in settings.INSTALLED_APPS:
         settings.INSTALLED_APPS += (celeryapp,)
-    
-    urlpatterns += [
-        url(r"^", include("geonode_sos.api.urls")),
-    ]
-    url_already_injected = any(
-        [
-            'geonode_sos.sensors.urls' in x.urlconf_name.__name__
-            for x in urlpatterns
-            if hasattr(x, 'urlconf_name') and not isinstance(x.urlconf_name, list)
-        ]
-    )
-
-    if not url_already_injected:
-        urlpatterns.insert(
-            0, url(r"^layers/", include("geonode_sos.sensors.layer_urls")),
-        )
-        urlpatterns.insert(
-            1, url(r"^services/", include("geonode_sos.sensors.service_urls")),
-        )
 
 
 class AppConfig(BaseAppConfig):
--- src/geosk/geonode_sos/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig as BaseAppConfig
-
-
-class AppConfig(BaseAppConfig):
-    name = 'geosk.geonode_sos'
-    label = 'geosk_geonode_sos'
-
-
-default_app_config = 'geosk.geonode_sos.AppConfig'
